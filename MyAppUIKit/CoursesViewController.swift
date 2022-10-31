f4293b8d4d90eba1f937afd6cdefb0ca6e209405
Angelica dos Santos <santosangelicassp@gmail.com>
2022-10-31 19:46:18 -0300

    setup tableview

--- MyAppUIKit/CoursesViewController.swift
+++ MyAppUIKit/CoursesViewController.swift
@@ -11,9 +11,15 @@ public protocol DataFetchable {
     func fetchCoursesNames(completion: @escaping ([String]) -> Void)
 }
 
-public class CoursesViewController: UIViewController {
+public class CoursesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
 
-    let dataFetchable: DataFetchable
+    private let dataFetchable: DataFetchable
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
     
     public init(dataFetchable: DataFetchable) {
         self.dataFetchable = dataFetchable
@@ -30,9 +36,31 @@ public class CoursesViewController: UIViewController {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBackground
+        view.addSubview(tableView)
+        tableView.dataSource = self
+        tableView.delegate = self
         
         dataFetchable.fetchCoursesNames { [weak self] names in
             self?.courses = names.map { Course(name: $0) }
+            DispatchQueue.main.async {
+                self?.tableView.reloadData()
+            }
         }
     }
+    
+    public override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return courses.count
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = courses[indexPath.row].name
+        return cell
+    }
+    
 }
